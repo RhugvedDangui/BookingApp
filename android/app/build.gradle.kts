f331f05349586b5aa30f007b78ca92a850b5df07
RhugvedDangui <rhugveddangui07@gmail.com>
2025-03-17 10:29:25 +0530

    before gradle change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     namespace = "com.example.test01"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
